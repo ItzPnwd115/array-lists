30d2ba8724132bd3782e7c8caf55263b4f8334de
Donlin <sdonli4894@greenville.k12.sc.us>
2018-01-29 10:18:45 -0500

    updated

--- ArrayListPractice.java
+++ ArrayListPractice.java
@@ -14,7 +14,7 @@ public class ArrayListPractice
 		ArrayList<String> letters = new ArrayList<String>();
 		
 		// #0
-		System.out.println(letters.size() + " " + letters.isEmpty());
+		System.out.println(letters.size() + " " + letters.isEmpty());//0 true
 								
 		// #1
 		letters.add("a");
@@ -22,32 +22,32 @@ public class ArrayListPractice
 		letters.add("c");
 		letters.add("d");
 
-		System.out.println(letters.size() + " " + letters.isEmpty());
+		System.out.println(letters.size() + " " + letters.isEmpty());// 4 false
 
 		// #2 
-		System.out.println(letters);
-		System.out.println(letters.get(3));
+		System.out.println(letters);//abcd
+		System.out.println(letters.get(3));//d
 
 		// #3
 		letters.add("e");
 		letters.add(2,"x");
-		System.out.println(letters);
+		System.out.println(letters);//zbxe
 				
 		// #4
 		letters.set(0,"z");
 		letters.remove("c");	
 		letters.remove(3);
-		System.out.println(letters);
+		System.out.println(letters);//zabde
 		
 		// #5
 		for (int i=letters.size()-1; i>=0; i--)
 			System.out.print(letters.get(i));
-		System.out.println();	
+		System.out.println();//zbxe	
 		
 		// #6
 		for (String let: letters)
 			System.out.print(let);
-		System.out.println();
+		System.out.println();//exbz
 			
 		// #7
 		ArrayList<String> more = new ArrayList<String>();
@@ -58,6 +58,6 @@ public class ArrayListPractice
 		for (String let : more)
 			letters.add(0, let);
 			
-		System.out.println(letters);
+		System.out.println(letters);//srqzbxe
 	}
 }
